c8665a2ad54045866f35e46e42dcd5eac22e39d3
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-03 15:47:08 +0200

    Add signupform event and error handlers

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
-    <div className="styles.App">
+    <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin/" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup/" render={() => <SignUpForm />} />
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://moments-ek.herokuapp.com/";
+axios.defaults.baseURL = "https://social-media-api-walkthrough.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
